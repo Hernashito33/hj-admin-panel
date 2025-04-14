26f9e38680163ade21af35b43e4f3f9c442bd11f
Hernashito33 <hernanmontalvosierra@gmail.com>
2025-04-14 17:16:15 -0500

    app.py

--- app.py
+++ app.py
@@ -53,4 +53,6 @@ def cambiar_estado(id):
     return redirect('/admin')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
+
